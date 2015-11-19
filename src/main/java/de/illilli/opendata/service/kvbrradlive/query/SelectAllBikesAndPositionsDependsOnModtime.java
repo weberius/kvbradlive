5e06ca5e79ccaa9fca353cb30a5ec578b76af1d1
Wolfram <eberius@gmail.com>
2015-11-19 05:22:37 +0100

    add comment

--- src/main/java/de/illilli/opendata/service/kvbrradlive/query/SelectAllBikesAndPositionsDependsOnModtime.java
+++ src/main/java/de/illilli/opendata/service/kvbrradlive/query/SelectAllBikesAndPositionsDependsOnModtime.java
@@ -7,10 +7,22 @@ import java.sql.Timestamp;
 import javax.naming.NamingException;
 
 /**
- * Diese Abfrage gegen die Datenbank
+ * Mit dieser Abfrage werden alle Fahrraddaten abgefragt, von Fahrrädern, deren
+ * Position sich nach dem Zeitstempel geändert hat. Es werden alle
+ * Fahrradpositionen zurückgegeben unabhängig davon, ob sie vor oder nach dem
+ * übergebenen Zeitstempel in die Datenbank geschrieben werden.
  */
 public class SelectAllBikesAndPositionsDependsOnModtime extends SelectBike {
 
+	/**
+	 * 
+	 * @param time
+	 *            Zeitstempel in Form eines long zur Einrschränkung der
+	 *            Rückgabe.
+	 * @throws SQLException
+	 * @throws NamingException
+	 * @throws IOException
+	 */
 	public SelectAllBikesAndPositionsDependsOnModtime(long time)
 			throws SQLException, NamingException, IOException {
 
