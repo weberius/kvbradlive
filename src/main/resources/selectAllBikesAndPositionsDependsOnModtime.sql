b846639e720abdac86685e11f7b8e5c9611ec36c
Wolfram <eberius@gmail.com>
2015-12-24 07:42:04 +0100

    comment

--- src/main/resources/selectAllBikesAndPositionsDependsOnModtime.sql
+++ src/main/resources/selectAllBikesAndPositionsDependsOnModtime.sql
@@ -1,16 +1,13 @@
+-- selectiert alle bikes die sich nach dem zeitpunkt veraendert haben 
 select 
   uid, name, bike, number, modtime, geom
 from 
   bike
 where 
   number in (
-    select 
-      distinct number
-    from 
-      bike
-    where 
-      modtime > ?
+    select distinct number
+    from bike
+    where modtime > ?
   )
 order by
   number, modtime desc
-;
